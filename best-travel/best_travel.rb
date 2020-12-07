7f563f1bbaf56fe276adcc8ef147274b64ec50fa
95-Samb <samuelbrown201195@hotmail.co.uk>
2020-12-07 15:17:36 +0000

    deleted unnecessary to_a method

--- best-travel/best_travel.rb
+++ best-travel/best_travel.rb
@@ -1,7 +1,7 @@
 class BestTravel
 
   def execute(t,k,ls)
-    possible_routes = ls.combination(k).to_a
+    possible_routes = ls.combination(k)
     possible_journeys = possible_routes.map(&:sum).delete_if { |e| e > t  }
     possible_journeys.max
   end
